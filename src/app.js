4a7317db5d0dd1a8adc64e7cc698d8dd8690855e
tHe-novice793 <ankitbhoi793@gmail.com>
2025-08-08 01:21:47 +0530

    Add Some New API

--- src/app.js
+++ src/app.js
@@ -7,19 +7,68 @@ const app = express();
 const User = UserModel;
 // const connectDB = connectDB();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Mario",
-    lastName: "Doe",
-    emailId: "Mariodoe@gmail.com",
-    password: "Mario@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added successfully");
   } catch (error) {
     res.status(400).send("Error while saving the user:" + err.message);
+  }
+});
+
+// Feed API - GET/feed - To get all the users from the database
+app.get("/feed", async (req, res) => {
+  // const userEmailId = req.body.emailId;
+
+  try {
+    const users = await User.find({});
+    if (!users.length) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!!", err.message);
+  }
+});
+
+// Delete API - DELETE/user - To delete the user
+app.delete("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  console.log(userEmail);
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+    await User.findByIdAndDelete(user._id);
+    res.send("User is deleted successfully.");
+  } catch (err) {
+    res.status(400).send("Something went wrong !!");
+  }
+});
+
+// Update API - PATCH/user - To update the user
+app.patch("/user", async (req, res) => {
+  console.log("Received body:", req.body);
+  const { emailId, ...updateFields } = req.body;
+  try {
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+    const updatedUser = await User.findByIdAndUpdate(user._id, updateFields, {
+      new: true,
+    });
+    res
+      .status(200)
+      .json({ message: "User is updated successfully.", user: updatedUser });
+  } catch (err) {
+    res.status(400).send("Something went wrong !!");
   }
 });
 
